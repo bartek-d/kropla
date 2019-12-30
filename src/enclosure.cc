f7aa2a6229712156a50b825aa8fefb41eb42bb1a
BD <bdyda@protonmail.com>
2019-12-30 23:17:50 +0100

    Enclosure moved to separate file

---
+++ src/enclosure.cc
@@ -0,0 +1,148 @@
+/********************************************************************************************************
+ kropla -- a program to play Kropki; file enclosure.cc.
+    Copyright (C) 2015,2016,2017,2018 Bartek Dyda,
+    email: bartekdyda (at) protonmail (dot) com
+
+    This file is part of Kropla.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************************************/
+
+#include "enclosure.h"
+#include <cassert>
+#include <algorithm>
+
+Enclosure empty_enclosure;
+
+bool
+Enclosure::isInInterior(pti p) const
+{
+  // return (std::binary_search(interior.begin(), interior.end(), p));
+  // if (p <= border.front()) return false;  // to speed up on average
+  return (std::find(interior.begin(), interior.end(), p)!=interior.end() );
+}
+
+bool
+Enclosure::isInBorder(pti p) const
+{
+  return (std::find(border.begin(), border.end(), p)!=border.end() );
+}
+
+bool
+Enclosure::isEmpty() const
+{
+  return border.empty();
+}
+
+// returns some element on the border
+pti
+Enclosure::getBorderElement() const
+{
+  return border.front();
+}
+
+// returns some element in the interior
+pti
+Enclosure::getInteriorElement() const
+{
+  return interior.front();
+}
+
+int
+Enclosure::getInteriorSize() const
+{
+  return interior.size();
+}
+
+uint64_t
+Enclosure::zobristKey(int who) const
+{
+  uint64_t key = 0;
+  who--;
+  for (auto &p : interior) {
+    key ^= coord.zobrist_encl[who][p];
+  }
+  return key;
+}
+
+/// Given a point p1 from the border and a point x!=p1 where the player just played,
+/// it finds previous (p0) and next (p2) point from the border and checks,
+/// whether x touches p0, p1 and p2. In particular, it may happen that p1==x.
+//  If yes, there is a shortcut.
+/// @param[in] p1  Point from the border.
+/// @param[in] x   Point where the player just played.
+/// @return    bool value
+bool
+Enclosure::checkShortcut(pti p1, pti x) const
+{
+  //if (p1==x) return true;
+  if (!coord.isInNeighbourhood(p1, x)) return false;
+  assert(border[0] == border[border.size()-1]);
+  int nr=0;
+  while (border[nr] != p1 && nr<border.size()) nr++;
+  if (nr > 0) {
+    assert(nr+1 < border.size());  // this should hold because border[0] == border[border.size()-1]
+    return (coord.isInNeighbourhood(x, border[nr-1]) && coord.isInNeighbourhood(x, border[nr+1]));
+  } else {
+    return (coord.isInNeighbourhood(x, border[1]) && coord.isInNeighbourhood(x, border[border.size()-2]));
+  }
+}
+
+/// Given a point p1 from the border,
+/// it finds previous (p0) and next (p2) point from the border and checks,
+/// whether p0 touches p2.
+//  If yes, then p1 is redundant.
+/// @param[in] p1  Point from the border.
+/// @return    bool value
+bool
+Enclosure::checkIfRedundant(pti p1) const
+{
+  assert(border[0] == border[border.size()-1]);
+  int nr=0;
+  while (border[nr] != p1 && nr<border.size()) nr++;
+  if (nr > 0) {
+    assert(nr+1 < border.size());  // this should hold because border[0] == border[border.size()-1]
+    return coord.isInNeighbourhood(border[nr-1], border[nr+1]);
+  } else {
+    return coord.isInNeighbourhood(border[1], border[border.size()-2]);
+  }
+}
+
+
+std::string
+Enclosure::show() const
+{
+  std::vector<pti> tab(coord.getSize(), 0);
+  for (auto &e : border) tab[e] = 1;
+  for (auto &e : interior) tab[e] |= 2;
+  std::stringstream out;
+  out << "Border = ";
+  for (auto e : border) out << coord.showPt(e) << " ";
+  out << std::endl;
+  return coord.showBoard(tab) + out.str();
+}
+
+std::string
+Enclosure::toSgfString() const
+{
+  std::string es;
+  assert(!border.empty());
+  for (auto &p : border)
+    es += coord.indToSgf(p);
+  if (border[0] < border[1])
+    return "." + es;
+  else
+    return "." + es.substr(2) + es.substr(2,2);  // could be always "."+es, but we want to save in a zagram.org's style
+}
+
---
+++ src/enclosure.h
@@ -0,0 +1,60 @@
+/********************************************************************************************************
+ kropla -- a program to play Kropki; file enclosure.cc.
+    Copyright (C) 2015,2016,2017,2018 Bartek Dyda,
+    email: bartekdyda (at) protonmail (dot) com
+
+    This file is part of Kropla.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************************************/
+
+#pragma once
+
+#include "board.h"
+#include <vector>
+#include <string>
+
+/********************************************************************************************************
+  Enclosure class
+*********************************************************************************************************
+  There are two lists: interior, border with the list of interior, border points of the enclosure.
+  (getInteriorSize() == 0) indicates no enclosure (empty enclosure).
+*********************************************************************************************************/
+struct Enclosure {
+  std::vector<pti> interior;
+  std::vector<pti> border;
+  Enclosure(std::vector<pti> const &i, std::vector<pti> const &b) : interior(i), border(b) {};
+  Enclosure(std::vector<pti> &&i, std::vector<pti> &&b) : interior(std::move(i)), border(std::move(b)) {};
+  /* set version seems to be slower:
+  std::set<pti,std::greater<pti>> interior;
+  std::set<pti,std::greater<pti>> border;
+
+  Enclosure(std::vector<pti> i, std::vector<pti> b) : interior(i.begin(), i.end()), border(b.begin(), b.end()) {};
+  */
+  Enclosure() = default;
+  ~Enclosure() = default;
+  bool isInInterior(pti p) const;
+  bool isInBorder(pti p) const;
+  bool isEmpty() const;
+  pti getBorderElement() const;
+  pti getInteriorElement() const;
+  int getInteriorSize() const;
+  uint64_t zobristKey(int who) const;
+  bool checkShortcut(pti p1, pti x) const;
+  bool checkIfRedundant(pti p1) const;
+  std::string show() const;
+  std::string toSgfString() const;
+};
+
+extern Enclosure empty_enclosure;
