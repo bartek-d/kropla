3b46b85db9a65cb5c3198ab7851c6faa79fabf8f
Bartek Dyda <bartekdyda@protonmail.com>
2021-04-06 00:48:22 +0200

    CNN seems to work in kropla

---
+++ src/get_cnn_prob.cc
@@ -0,0 +1,119 @@
+/********************************************************************************************************
+ kropla -- a program to play Kropki; file get_cnn_prob.cc -- reading NN.
+    Copyright (C) 2021 Bartek Dyda,
+    email: bartekdyda (at) protonmail (dot) com
+
+    Some parts are inspired by Pachi http://pachi.or.cz/
+      by Petr Baudis and Jean-loup Gailly
+
+    This file is part of Kropla.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************************************/
+
+#include "get_cnn_prob.h"
+#include "caffe/mcaffe.h"
+
+#include <boost/multi_array.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <mutex>
+
+namespace {
+MCaffe cnn;
+std::mutex caffe_mutex; 
+constexpr int PLANES = 10;
+
+using Array3dim = boost::multi_array<float, 3>;
+}
+
+std::pair<bool, std::vector<float>> getCnnInfo(Game& game) try
+{
+  Array3dim data{boost::extents[PLANES][coord.wlkx][coord.wlky]};
+  const std::lock_guard<std::mutex> lock(caffe_mutex);
+  if (not cnn.caffe_ready()) {
+    std::string model_file_name;
+    std::string weights_file_name;
+    std::ifstream t("cnn.config");
+    if (std::getline(t, model_file_name))
+      std::getline(t, weights_file_name);
+    cnn.caffe_init(coord.wlkx, model_file_name, weights_file_name, coord.wlky);
+  }
+  for (int x = 0; x < coord.wlkx; ++x)
+    for (int y = 0; y < coord.wlky; ++y) {
+      int p = coord.ind(x, y);
+      int on_move = game.whoNowMoves();
+      int opponent = 3 - on_move;
+      data[0][x][y] = (game.whoseDotMarginAt(p) == 0) ? 1.0f : 0.0f;
+      data[1][x][y] = (game.whoseDotMarginAt(p) == on_move) ? 1.0f : 0.0f;
+      data[2][x][y] = (game.whoseDotMarginAt(p) == opponent) ? 1.0f : 0.0f;
+      data[3][x][y] = game.isInTerr(p, on_move) > 0 ? 1.0f : 0.0f;
+      data[4][x][y] = game.isInTerr(p, opponent) > 0 ? 1.0f : 0.0f;
+      data[5][x][y] = std::min(game.isInEncl(p, on_move), 2) * 0.5f;
+      data[6][x][y] = std::min(game.isInEncl(p, opponent), 2) * 0.5f;
+      data[7][x][y] = std::min(game.isInBorder(p, on_move), 2) * 0.5f;
+      data[8][x][y] = std::min(game.isInBorder(p, opponent), 2) * 0.5f;
+      data[9][x][y] = std::min(game.getTotalSafetyOf(p), 2.0f) * 0.5f;
+      //	data[10][x][y] = (coord.dist[p] == 1) ? 1 : 0;
+      //	data[11][x][y] = (coord.dist[p] == 4) ? 1 : 0;
+    }
+  
+  auto res = cnn.caffe_get_data(static_cast<float*>(&data[0][0][0]), coord.wlkx, PLANES, coord.wlky);
+  std::vector<float> probs(coord.getSize(), 0.0f);
+  for (int x = 0; x < coord.wlkx; ++x) {
+    for (int y = 0; y < coord.wlky; ++y) {
+      probs[coord.ind(x, y)] = res[x * coord.wlky + y];
+    }
+  }
+  return {true, probs};
+}
+catch (const CaffeException& exc) {
+  std::cerr << "Failed to load cnn" << std::endl;
+  return {false, {}};
+}
+
+void updatePriors(Game& game, Treenode* children)
+{
+  const auto [is_cnn_available, probs] = getCnnInfo(game);
+  std::cerr << "Trying to update priors from CNN: " << is_cnn_available << std::endl;
+  if (not is_cnn_available) return;
+  float max = 0.0f;
+  for (auto *ch = children; true; ++ch) {
+    if (not ch->isDame() and probs[ch->move.ind] > max) {
+      max = probs[ch->move.ind];
+    }
+    if (ch->isLast()) break;
+  }
+  if (max == 0.0f) {
+    std::cerr << "Max is 0.0f, CNN does not work?" << std::endl;
+    return;
+  }
+  constexpr float prior_for_best = 200.0f;
+  for (auto *ch = children; true; ++ch) {
+    float prob = probs[ch->move.ind];
+    if (not ch->isDame()) {
+      prob = std::sqrt(prob);
+    } else {
+      prob *= prob;
+    }
+    const int32_t value = prob * prior_for_best;
+    ch->t.playouts += value;
+    ch->t.value_sum = ch->t.value_sum.load() + value;
+    if (ch->isLast()) break;
+  }
+  std::cerr << "Updated priors from CNN!" << std::endl;
+}
---
+++ src/get_cnn_prob_dummy.cc
@@ -0,0 +1,33 @@
+/********************************************************************************************************
+ kropla -- a program to play Kropki; file get_cnn_prob_dummy.cc
+    Copyright (C) 2021 Bartek Dyda,
+    email: bartekdyda (at) protonmail (dot) com
+
+    Some parts are inspired by Pachi http://pachi.or.cz/
+      by Petr Baudis and Jean-loup Gailly
+
+    This file is part of Kropla.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************************************/
+
+#include "get_cnn_prob.h"
+
+std::pair<bool, std::vector<float>> getCnnInfo(Game& game)
+{
+  return {false, {}};
+}
+
+void updatePriors(Game& game, Treenode* children)
+{}
