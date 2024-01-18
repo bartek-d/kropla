57bf329fc59bbeb6c25399e24cf64ff1eed69c56
Bartek Dyda <bartekdyda@protonmail.com>
2024-01-18 20:07:32 +0100

    Add UT checking extracting moves from both LB and B/W properties.

--- unittest/extractutils-test.cc
+++ unittest/extractutils-test.cc
@@ -86,6 +86,83 @@ TEST(ExtractUtilsTest, extractsMove)
         << "actual: " << actual << "\nexpected: " << expected_data << std::endl;
 }
 
+TEST(ExtractUtilsTest, extractsMovesFromLB)
+{
+    const std::string s{
+        "(;SZ[6];W[ab];B[df];W[ac];B[ee];W[fc]LB[fc:17][ec:8][dc:1];B[ad]LB[ad:"
+        "12][ae:1][af:10];W[ce];B[ed])"};
+    const std::string expected_data{
+        ".11B.b"
+        "......"
+        "....C."
+        ".....2"
+        "..a.2."
+        "..A..."};
+    constexpr int MOVES_USED = 3;
+    constexpr int PLANES = 20;
+    constexpr int BSIZEX = 6;
+    constexpr int BSIZEY = 6;
+    const std::vector<int> expected1{1, PLANES, BSIZEX, BSIZEY};
+    const std::vector<int> expected2{1, MOVES_USED, BSIZEX, BSIZEY};
+    int run = 0;
+    std::string actual;
+    auto tensorSaver = [&run, &expected1, &expected2, &actual](
+                           float* ptr, const std::string& /*filename*/, int a,
+                           int b, int c, int d)
+    {
+        if (run == 0)
+        {
+            EXPECT_EQ(expected1, (std::vector<int>{a, b, c, d}));
+            actual = std::string(a * c * d, '.');
+            for (int sample = 0; sample < a; ++sample)
+                for (int plane : {1, 2})
+                    for (int i = 0; i < c; ++i)
+                        for (int j = 0; j < d; ++j)
+                            if (ptr[sample * b * c * d + plane * d * c + i * d +
+                                    j] >= 0.99f)
+                                actual[sample * c * d + i * d + j] =
+                                    plane == 1 ? '1' : '2';
+            run = 1;
+        }
+        else
+        {
+            EXPECT_EQ(expected2, (std::vector<int>{a, b, c, d}));
+            for (int move_no = 0; move_no < b; ++move_no)
+            {
+                const char marker = 'a' + move_no;
+                for (int sample = 0; sample < a; ++sample)
+                    for (int i = 0; i < c; ++i)
+                    {
+                        for (int j = 0; j < d; ++j)
+                        {
+                            if (ptr[sample * b * c * d + move_no * d * c +
+                                    i * d + j] >= 0.5f)
+                                actual[sample * c * d + i * d + j] =
+                                    marker + 'A' - 'a';
+                            else if (ptr[sample * b * c * d + move_no * d * c +
+                                         i * d + j] >= 0.01f)
+                                actual[sample * c * d + i * d + j] = marker;
+                        }
+                    }
+            }
+        }
+    };
+    SgfParser parser(s);
+    {
+        auto compressed_data = std::make_unique<
+            CompressedData<MOVES_USED, PLANES, BSIZEX, BSIZEX>>(tensorSaver);
+        auto seq = parser.parseMainVar();
+        const bool must_surround = false;
+        const bool blueOk = true;
+        const bool redOk = true;
+        gatherDataFromSgfSequence(*compressed_data, seq,
+                                  {{1, blueOk}, {2, redOk}}, must_surround);
+        compressed_data->dump();
+    }
+    EXPECT_EQ(expected_data, actual)
+        << "actual: " << actual << "\nexpected: " << expected_data << std::endl;
+}
+
 TEST(ExtractUtilsTest, getMovesFromSgfNodePropertyLB)
 {
     const std::string sgf{
